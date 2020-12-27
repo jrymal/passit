dd653a55cfec928918f2baa5c54a8fbb8ebf4e09
Jason Rymal <jasonrymal@gmail.com>
2020-12-26 22:20:50 -0500

    initil pass of things

---
+++ js/MidiNotes.js
@@ -0,0 +1,348 @@
+"use strict";
+
+const BASELINE_IDX = 9;
+const BASELINE_NOTE = "A";
+const BASELINE_OCT = 4;
+const BASELINE_FREQ = 440;
+
+const NOTES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+const VCO = {
+    init: function(context){
+        this.context = context;
+        
+        this.oscillator = context.createOscillator();
+
+        this.oscillator.type = 'sawtooth';
+        
+        this.input = this.oscillator;
+        this.output = this.oscillator;
+        this.frequency = this.oscillator.frequency;
+        return this;
+    },
+
+    setFrequency: function(freq) {
+        this.oscillator.frequency.setValueAtTime(freq, this.context.currentTime);
+    },
+      
+    // sine, triangle, custom, square, sawtooth
+    setType: function(type) {
+
+        this.oscillator.type = type;
+    },
+
+    start: function(){
+        if (!this.started){
+            this.started = true;
+            this.oscillator.start(0);
+        }
+    },
+
+    setCustomType: function(sineTermList=[0, 0, 1, 0, 1]) {
+        let sineTerms = new Float32Array(sineTermList);
+        let cosineTerms = new Float32Array(this.sineTerms.length);
+        let customWaveform = this.audioContext.createPeriodicWave(this.cosineTerms, this.sineTerms);
+        // this will convert it to custom automatically
+        this.oscillator.setPeriodicWave(customWaveform);
+    },
+
+    connect: function(node) {
+        if (node.hasOwnProperty('input')) {
+          this.output.connect(node.input);
+        } else {
+          this.output.connect(node);
+        }
+    },
+};
+
+const EnvelopeGenerator = {
+    init: function(context) {
+        this.context = context;
+        
+        // time to full volume
+        this.attackTime = 0.01;
+        
+        // time to full quiet after decay time
+        this.releaseTime = 0.1;
+        
+        // value to first drop
+        this.decayValue = 0.35;
+        
+        // time to first drop
+        this.decayTime = 0.35;
+        
+        // peak value
+        this.maxValue = 0.9;
+
+        // time remaining at max value
+        this.maxTime = .1;
+
+        // time remaining at decay value
+        this.sustainTime = 0.1;
+        return this;
+    },
+    setVolume: function(value){
+        this.maxValue = 0.9 * (value/100);
+        this.decayValue = 0.35 * (value/100);
+    },
+
+    setAttack: function(value){
+        this.attackTime = value;
+    },
+    
+    setRelease: function(value){
+        this.releaseTime= value;
+    },
+
+    trigger: function() {
+        let now = this.context.currentTime;
+        this.param.cancelScheduledValues(now);
+        
+        this.param.setValueAtTime(0, now);
+        this.param.linearRampToValueAtTime(this.maxValue,   now + this.attackTime);
+        this.param.linearRampToValueAtTime(this.maxValue,   now + this.attackTime + this.maxTime);
+        this.param.linearRampToValueAtTime(this.decayValue, now + this.attackTime + this.maxTime + this.decayTime);
+        this.param.linearRampToValueAtTime(this.decayValue, now + this.attackTime + this.maxTime + this.decayTime + this.sustainTime);
+        this.param.linearRampToValueAtTime(0,               now + this.attackTime + this.maxTime + this.decayTime + this.sustainTime + this.releaseTime);
+    },
+
+    getTotalTime: function() {
+        return this.attackTime + this.maxTime + this.decayTime + this.sustainTime + this.releaseTime;
+    },
+
+    connect: function(param) {
+        this.param = param;
+    },
+};
+
+const RelativeRNG = {
+    init: function(context) {
+        this.context = context;
+        
+        this.centMin = 0.025;
+        this.centMax = 0.025;
+        this.changeCnt = 30;
+        
+        return this;
+    },
+
+    trigger: function(targetValue, totalTime) {
+        let now = this.context.currentTime;
+        this.param.cancelScheduledValues(now);
+        
+        let origValue = targetValue;
+        let min = origValue - (origValue * this.centMin);
+        let max = origValue + (origValue * this.centMax);
+
+        let runningTime = now;
+        this.param.linearRampToValueAtTime(origValue, runningTime);
+        for(let i = 1; i < this.changeCnt; i++){
+            let value = this.getRNGValue(min, max);
+            runningTime = i*totalTime/this.changeCnt;
+            this.param.linearRampToValueAtTime(value, runningTime);
+        }
+        this.param.linearRampToValueAtTime(origValue, runningTime);
+    },
+
+    getRNGValue: function(min, max){
+        return (Math.random() * (max - min)) + min;
+    },
+
+    connect: function(param) {
+        this.param = param;
+    },
+};
+const VCA = {
+    init: function(context) {
+        this.gain = context.createGain();
+        this.gain.gain.value = 1;
+        this.input = this.gain;
+        this.output = this.gain;
+        this.amplitude = this.gain.gain;
+        return this;
+    },
+
+    setVolume: function(value){
+        this.gain.gain.value = value/100;
+    },
+
+    connect: function(node) {
+        if (node.hasOwnProperty('input')) {
+          this.output.connect(node.input);
+        } else {
+          this.output.connect(node);
+        }
+    },
+};
+
+const VOICE = {
+    init: function(context){
+    
+        let vco = Object.create(VCO).init(context);
+        let vca = Object.create(VCA).init(context);
+        let envelope = Object.create(EnvelopeGenerator).init(context);
+        let pitchControl = Object.create(RelativeRNG).init(context);
+
+        vco.connect(vca);
+        envelope.connect(vca.amplitude);
+        pitchControl.connect(vco.frequency);
+        vca.connect(context.destination);
+        
+
+        this.audioContext = context;
+        this.vco = vco;
+        this.vca = vca;
+        this.envelope = envelope;
+        this.pitchControl = pitchControl;
+        
+        return this;
+    },
+
+    playFreq: function(freq) {
+        this.stop();
+        this.vco.start();
+
+        // controls the freq, mutually exclusive
+        //this.vco.setFrequency(freq);
+        this.pitchControl.trigger(freq, this.envelope.getTotalTime());
+        
+        // starts the sound
+        this.envelope.trigger();
+    },
+    stop: function(){
+        this.vca.setVolume(0);
+    },   
+    setVolume: function(volume){
+        this.vca.setVolume(volume);
+        this.envelope.setVolume(volume);
+    }   
+};
+
+const VOICE_LIST = {
+    init: function(context, count){
+        this.voiceList = [];
+        this.lastPlayed = -1;
+
+        for(let i = 0; i < count; i++){
+            this.voiceList.push(Object.create(VOICE).init(context));
+        }
+
+        return this;
+    },
+    playFreq: function(freq) {
+        let curVoice= (this.lastPlayed + 1) % this.voiceList.length;
+        this.voiceList[curVoice].playFreq(freq);
+        this.lastPlayed = curVoice;
+    },
+    stop: function(){
+        for(let i = 0; i < this.voiceList.length; i++){
+            this.voiceList[i].stop();
+        }
+    },
+    setVolume : function(volume){
+        for(let i = 0; i < this.voiceList.length; i++){
+            this.voiceList[i].setVolume(volume);
+        }
+    },
+    setProperty: function(functionality, propertyName, value, voiceIdx){
+        if (!exists(voiceIdx)){
+            for(let i = 0; i < this.voiceList.length; i++){
+                this.voiceList[i][functionality][propertyName] = value;
+            }
+        } else {
+            this.voiceList[voiceIdx][propertyName] = value;
+        }
+        return this;
+    }
+ 
+};
+
+const MIDI_PLAYER = {
+    init: function(voiceCount=1){
+        
+        let context =  new (window.AudioContext || window.webkitAudioContext);
+        this.voice = Object.create(VOICE_LIST).init(context, voiceCount);
+        this.space = 200;
+        this.volume = 100;
+        return this;
+    },
+
+    playNote:function(noteList, cnt=1, completedCallback=null) {
+
+        
+        if (!Array.isArray(noteList)){
+            noteList = [ noteList ];
+        }
+
+        let fullNoteList = [];
+        for( let i = cnt; i > 0; i--){
+            noteList.forEach(
+                note => {
+                    fullNoteList.push(note);
+                }
+            );
+        }
+
+        let player = Object.create(PLAY_LIST).init(this, fullNoteList, completedCallback);
+    },
+
+    setSpacing : function(space){
+        this.space = space;
+    },
+
+    setVolume: function(volume){
+        this.volume = volume;
+    },
+    
+    stop:function(oscMatch){
+        this.voice.stop();
+    },
+};
+
+const PLAY_LIST = {
+    init: function(midiPlayer, fullNoteList, completedCallback){
+        this.timerId = setInterval(this.playNoteList, midiPlayer.space, this, midiPlayer, fullNoteList);
+        this.completedCallback = completedCallback;
+    },
+    
+    playNoteList : function(playList, midiPlayer, noteList) {
+        let note = noteList.shift();
+        midiPlayer.voice.setVolume(midiPlayer.volume);
+        if (note){
+            midiPlayer.voice.playFreq(playList.noteFreq(note));
+        } else {
+            clearInterval(playList.timerId);
+            if (playList.completedCallback){
+                playList.completedCallback();
+            }
+        }
+    },
+
+    noteFreq : function(note){
+        let dist = this.findDistanceOfNote(note);
+        let freq = BASELINE_FREQ * Math.pow(2, dist/12);
+        return freq;
+    },
+
+    findDistanceOfNote : function(note){
+        let split = note.split(':');
+        let reqNote = split[0];
+        let reqOct = BASELINE_OCT;
+
+        if (split.length > 1){
+            reqOct = split[1];
+        }
+
+        // this unfortunately for this pass is dependent on MusicTheory.js
+        let idxReqNote = NOTES.indexOf(reqNote);
+
+        if (idxReqNote == -1){
+            console.log("Note not recognized:"+note);
+            return null;
+        }
+
+        let octDist = reqOct - BASELINE_OCT; 
+        let noteDist = idxReqNote - BASELINE_IDX;
+        return noteDist + (octDist * 12);
+    },
+};
---
+++ js/PassIt.js
@@ -0,0 +1,216 @@
+"use strict";
+
+const TimeGroups={
+    "very_short": {
+        min:30,
+        max:45
+    },
+    "short": {
+        min:40,
+        max:60
+    },
+    "medium": {
+        min:50,
+        max:90
+    },
+    "long": {
+        min:60,
+        max:180
+    },
+    "very_long": {
+        min:120,
+        max:300
+    },
+};
+
+const PASS_IT_APP = {
+    init: function (){
+        this.setState(history.state);
+        this.addUiListeners();
+        this.setVolume();
+    
+        show($("youAreIt"), false);
+        show($("start"), true);
+        show($("resume"), false);
+        show($("stop"), false);
+        show($("pause"), false);
+    },
+
+    setInstallPromptHandler: function(){
+        window.onbeforeinstallprompt = function(event){
+            // Prevent Chrome <= 67 from automatically showing the prompt
+            event.preventDefault();
+            // Stash the event so it can be triggered later.
+            app.installPromptEvent = event;
+
+            show($('install-app'), true);
+        };
+        return this;
+    },
+
+    setState: function(state){
+        if (state){
+            if (state["volume"]){
+                $("volume").value = state["volume"];
+            }
+            if (state["timer"]){
+                $("timer").value = state["timer"];
+            }
+        }
+    },
+
+    getState: function(){
+        return {
+            "volume":$("volume").value,
+            "timer":$("timer").value,
+        };
+    },
+
+    addUiListeners: function(){
+        var self = this;
+        $("volume").addEventListener("change", () => self.setVolume());
+        $("timer").addEventListener("change", () => self.updateAll());
+        $("install-app").addEventListener("click", () => self.installApp());
+        $("start").addEventListener("click", () => self.startTimer());
+        $("resume").addEventListener("click", () => self.resumeTimer());
+        $("stop").addEventListener("click", () => self.stopTimer());
+        $("pause").addEventListener("click", () => self.pauseTimer());
+    },
+
+    setVolume : function(){
+        this.saveState();
+        let mp = this.getMidiPlayer();
+        mp.setVolume($("volume").value);
+    },
+
+    installApp: function(){
+        show($('install-app'), false);
+      
+        // Show the modal add to home screen dialog
+        this.installPromptEvent.prompt();
+        // Wait for the user to respond to the prompt
+        this.installPromptEvent.userChoice.then((choice) => {
+            if (choice.outcome === 'accepted') {
+              console.debug('User accepted the A2HS prompt');
+            } else {
+              console.debug('User dismissed the A2HS prompt');
+            }
+            // Clear the saved prompt since it can't be used again
+            this.installPromptEvent = null;
+        });
+    },
+
+    saveState: function() {
+        history.replaceState( this.getState(), window.title, window.location);
+    },
+
+    startTimer: function() {
+        var self = this
+        if (!exists(self.timer)) {
+            self.timer = Object.create(TIMER)
+                .init(() => self.gameComplete(),self.getRandomTime())
+                .start();
+        }
+        show($("youAreIt"), false);
+        show($("start"), false);
+        show($("resume"), false);
+        show($("stop"), true);
+        show($("pause"), true);
+
+        $("rootNode").classList.add("running");
+        $("rootNode").classList.remove("paused");
+        $("rootNode").classList.remove("complete");
+    },
+
+    resumeTimer: function(){
+        var self = this
+        if (exists(self.timer)) {
+            self.timer.resume();
+        } else {
+            self.timer = Object.create(TIMER)
+                .init(() => self.gameComplete(),self.getRandomTime())
+                .start();
+        }
+        show($("youAreIt"), false);
+        show($("start"), false);
+        show($("resume"), false);
+        show($("stop"), true);
+        show($("pause"), true);
+
+        $("rootNode").classList.add("running");
+        $("rootNode").classList.remove("paused");
+        $("rootNode").classList.remove("complete");
+
+    },
+
+    stopTimer:function(){
+        if (exists(this.timer)) {
+            this.timer.stop();
+            this.timer=null;
+        }
+        show($("youAreIt"), false);
+        show($("start"), true);
+        show($("resume"), false);
+        show($("stop"), false);
+        show($("pause"), false);
+        
+        $("rootNode").classList.remove("running");
+        $("rootNode").classList.remove("paused");
+        $("rootNode").classList.remove("complete");
+    },
+    pauseTimer:function(){
+        if (exists(this.timer)) {
+            this.timer.pause();
+        }
+        show($("youAreIt"), false);
+        show($("start"), false);
+        show($("resume"), true);
+        show($("stop"), true);
+        show($("pause"), false);
+        
+        $("rootNode").classList.remove("running");
+        $("rootNode").classList.add("paused");
+        $("rootNode").classList.remove("complete");
+    },
+
+    updateAll:function() {
+        this.saveState();
+    
+    },
+
+    getMidiPlayer: function(){
+        if (!exists(this.midiplayer)){
+            this.midiplayer = Object.create(MIDI_PLAYER).init(2);
+        }
+        return this.midiplayer;
+    },
+
+    getRandomTime:function(){
+        let tg = TimeGroups[$("timer").value];
+        return randomNumber(tg.min,tg.max)*1000;
+    },
+
+    gameComplete:function(){
+        this.stopTimer();
+        var mp = this.getMidiPlayer();
+        
+        show($("youAreIt"), true);
+        show($("start"), true);
+        show($("stop"), false);
+        show($("pause"), false);
+        
+        $("rootNode").classList.remove("running");
+        $("rootNode").classList.remove("paused");
+        $("rootNode").classList.add("complete");
+        
+        mp.playNote(["A:4", "A:5", "A:4"]);
+    }
+};
+
+/* HTML functions */
+const app = Object.create(PASS_IT_APP).setInstallPromptHandler();
+
+function init(){
+    app.init();
+}
+
